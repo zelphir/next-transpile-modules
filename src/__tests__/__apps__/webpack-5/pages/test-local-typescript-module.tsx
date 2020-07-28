b7457bab198015358f29c3530f09225c7c0b30cb
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2020-07-28 10:58:31 +0200

    Change webpack-5's pages folder to a symlink to basic's pages

--- src/__tests__/__apps__/webpack-5/pages/test-local-typescript-module.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { add } from 'shared-ts/utils/calc';
-
-const HomePage: React.FC = () => {
-  return <h1>The answer is {add(40, 3)}</h1>;
-};
-
-export default HomePage;
--- src/__tests__/__apps__/webpack-5/pages/test-npm-module.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import add from 'lodash-es/add';
-
-const HomePage = () => {
-  return <h1>The answer is {add(40, 4)}</h1>;
-};
-
-export default HomePage;
