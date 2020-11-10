af7c63e16b73017982bd3546e2f37ad9a003598d
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2020-11-10 15:02:40 +0100

    Re-add regexEqual tests

---
+++ src/__tests__/helpers.test.js
@@ -0,0 +1,18 @@
+const rewire = require('rewire');
+const anymatch = require('anymatch');
+
+const withTmRewire = rewire('../next-transpile-modules');
+
+const regexEqual = withTmRewire.__get__('regexEqual');
+
+describe('regexEqual', () => {
+  // Cannot test because of Jest https://github.com/facebook/jest/issues/2549
+  // test('should return true if two regexps are similar', () => {
+  //   expect(regexEqual(/a/, /a/)).toBe(true);
+  //   expect(regexEqual(/a/gi, /a/ig)).toBe(true);
+  // });
+
+  test('should return false if two regexps are different', () => {
+    expect(regexEqual(/a/, /[\\/]a[\\/]/)).toBe(false);
+  });
+});
