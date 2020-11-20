0dd6d5a6fb53f31bfeb67201066ccfcab42beb5d
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2020-11-20 18:48:01 +0100

    Fix Yarn Workspaces test setups + really candid Webpack 5 HMR

--- src/next-transpile-modules.js
+++ src/next-transpile-modules.js
@@ -160,6 +160,12 @@ const withTmInitializer = (modules = [], options = {}) => {
             use: options.defaultLoaders.babel,
             include: match,
           });
+
+          // IMPROVE ME: we are losing all the cache on node_modules, which is terrible
+          // The problem is managedPaths does not allow to isolate specific specific folders
+          config.snapshot = Object.assign(config.snapshot || {}, {
+            managedPaths: [],
+          });
         } else {
           config.module.rules.push({
             test: /\.+(js|jsx|mjs|ts|tsx)$/,
