d93ce873a88f1bb9a264fe7ab8b96f18a17ce8bd
Bogdan Chadkin <trysound@yandex.ru>
2020-12-13 19:24:59 +0300

    Replace pkg-dir with escalade
    Ref https://github.com/lukeed/escalade/
    
    pkg-dir and many other Sindre's modules are often bloated with
    dependencies. For example I have 4 instances of pkg-dir and every its
    dependency in my node_modules. Would be cool to replace them with
    something smaller like escalade which is gonna be used in
    webpack-plugin-serve, yargs and will be used in the next major
    jest release.

--- src/next-transpile-modules.js
+++ src/next-transpile-modules.js
@@ -2,7 +2,7 @@ const path = require('path');
 const process = require('process');
 
 const enhancedResolve = require('enhanced-resolve');
-const pkgDir = require('pkg-dir');
+const escalade = require('escalade/sync');
 
 // Use me when needed
 // const util = require('util');
@@ -51,6 +51,7 @@ const getPackageRootDirectory = (module) => {
   try {
     // Get the module path
     packageDirectory = resolve(CWD, module);
+    console.log('packageDirectory 2', packageDirectory);
 
     if (!packageDirectory) {
       throw new Error(
@@ -60,7 +61,15 @@ const getPackageRootDirectory = (module) => {
 
     try {
       // Get the location of its package.json
-      packageRootDirectory = pkgDir.sync(packageDirectory);
+      const pkgPath = escalade(packageDirectory, (dir, names) => {
+        if (names.includes('package.json')) {
+          return 'package.json';
+        }
+        return false;
+      });
+      if (pkgPath != null) {
+        packageRootDirectory = path.dirname(pkgPath);
+      }
     } catch (err) {
       throw new Error(
         `next-transpile-modules - an error happened when trying to get the root directory of "${module}". Is it missing a package.json?\n${err}`
