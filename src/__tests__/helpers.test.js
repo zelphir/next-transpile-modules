443b0e68c8115ec59144bccd4cd3739d7bd90ef4
Lukas Goebel <lukas.goebel@netlight.com>
2021-02-01 09:53:13 +0100

    Make tests more exhaustive

--- src/__tests__/helpers.test.js
+++ src/__tests__/helpers.test.js
@@ -20,7 +20,17 @@ describe('regexEqual', () => {
 
 describe('webpackMatcher', () => {
   test('should return correct value on Unix systems', () => {
-    const testStrings = ['test', '@mono/module', '@mono/sub/module', '@mono\\module', '@mono\\sub\\module'];
+    const testStrings = ['test', '@mono/module', '@mono/sub/module'];
+    const matcherInstance = webpackMatcher(testStrings);
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/test/test.ext'))).toBe(true);
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/module/foo'))).toBe(true);
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/sub/module/foo'))).toBe(true);
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/false/module'))).toBe(false);
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/false'))).toBe(false);
+  });
+
+  test('should return correct value on Unix systems', () => {
+    const testStrings = ['test', '@mono\\module', '@mono\\sub\\module'];
     const matcherInstance = webpackMatcher(testStrings);
     expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/test/test.ext'))).toBe(true);
     expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/module/foo'))).toBe(true);
@@ -31,6 +41,16 @@ describe('webpackMatcher', () => {
 
   test('should return correct value on Windows systems', () => {
     const testStrings = ['test', '@mono\\module', '@mono\\sub\\module'];
+    const matcherInstance = webpackMatcher(testStrings);
+    expect(matcherInstance('C:\\module\\node_modules\\test\\test.ext')).toBe(true);
+    expect(matcherInstance('C:\\module\\node_modules\\@mono\\module\\foo')).toBe(true);
+    expect(matcherInstance('C:\\module\\node_modules\\@mono\\sub\\module\\foo')).toBe(true);
+    expect(matcherInstance('C:\\module\\node_modules\\@mono\\false\\module')).toBe(false);
+    expect(matcherInstance('C:\\module\\node_modules\\@mono\\false')).toBe(false);
+  });
+
+  test('should return correct value on Windows systems', () => {
+    const testStrings = ['test', '@mono/module', '@mono/sub/module'];
     const matcherInstance = webpackMatcher(testStrings);
     expect(matcherInstance('C:\\module\\node_modules\\test\\test.ext')).toBe(true);
     expect(matcherInstance('C:\\module\\node_modules\\@mono\\module\\foo')).toBe(true);
