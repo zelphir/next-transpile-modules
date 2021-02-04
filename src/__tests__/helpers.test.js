8e5504a984bb996b6de163e76629665afb1cb64b
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2021-02-04 03:14:21 +0100

    Runs some tests conditionally

--- src/__tests__/helpers.test.js
+++ src/__tests__/helpers.test.js
@@ -1,6 +1,10 @@
 const rewire = require('rewire');
 const path = require('path');
 
+// TODO: remove me and mock path.sep instead
+const process = require('process');
+const testif = (condition) => (condition ? test : test.skip);
+
 const withTmRewire = rewire('../next-transpile-modules');
 
 const regexEqual = withTmRewire.__get__('regexEqual');
@@ -19,9 +23,10 @@ describe('regexEqual', () => {
 });
 
 describe('createWebpackMatcher', () => {
-  test('should return correct value on Unix systems', () => {
+  testif(process.platform !== 'win32')('should return correct value on Unix systems', () => {
     const testStrings = ['test', '@mono/module', '@mono/sub/module'];
     const matcherInstance = createWebpackMatcher(testStrings);
+
     expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/test/test.ext'))).toBe(true);
     expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/module/foo'))).toBe(true);
     expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/sub/module/foo'))).toBe(true);
@@ -29,18 +34,18 @@ describe('createWebpackMatcher', () => {
     expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/false'))).toBe(false);
   });
 
-  // These tests don't work as path.normalize normalizes `\\` to `\` and not `/` on Unix systems
-  // test('should return correct value on Unix systems', () => {
-  //   const testStrings = ['test', '@mono\\module', '@mono\\sub\\module'];
-  //   const matcherInstance = webpackMatcher(testStrings);
-  //   expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/test/test.ext'))).toBe(true);
-  //   expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/module/foo'))).toBe(true);
-  //   expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/sub/module/foo'))).toBe(true);
-  //   expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/false/module'))).toBe(false);
-  //   expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/false'))).toBe(false);
-  // });
+  testif(process.platform === 'win32')('should return correct value on Unix systems', () => {
+    const testStrings = ['test', '@mono\\module', '@mono\\sub\\module'];
+    const matcherInstance = createWebpackMatcher(testStrings);
 
-  test('should return correct value on Windows systems', () => {
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/test/test.ext'))).toBe(true);
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/module/foo'))).toBe(true);
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/sub/module/foo'))).toBe(true);
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/false/module'))).toBe(false);
+    expect(matcherInstance(path.normalize('/Users/User1/module/node_modules/@mono/false'))).toBe(false);
+  });
+
+  testif(process.platform !== 'win32')('should return correct value on Windows systems', () => {
     const testStrings = ['test', '@mono\\module', '@mono\\sub\\module'];
     const matcherInstance = createWebpackMatcher(testStrings);
     expect(matcherInstance(path.normalize('C:\\module\\node_modules\\test\\test.ext'))).toBe(true);
@@ -50,14 +55,14 @@ describe('createWebpackMatcher', () => {
     expect(matcherInstance(path.normalize('C:\\module\\node_modules\\@mono\\false'))).toBe(false);
   });
 
-  // These tests don't work as path.normalize normalizes `\\` to `\` and not `/` on Unix systems
-  // test('should return correct value on Windows systems', () => {
-  //   const testStrings = ['test', '@mono/module', '@mono/sub/module'];
-  //   const matcherInstance = webpackMatcher(testStrings);
-  //   expect(matcherInstance(path.normalize('C:\\module\\node_modules\\test\\test.ext'))).toBe(true);
-  //   expect(matcherInstance(path.normalize('C:\\module\\node_modules\\@mono\\module\\foo'))).toBe(true);
-  //   expect(matcherInstance(path.normalize('C:\\module\\node_modules\\@mono\\sub\\module\\foo'))).toBe(true);
-  //   expect(matcherInstance(path.normalize('C:\\module\\node_modules\\@mono\\false\\module'))).toBe(false);
-  //   expect(matcherInstance(path.normalize('C:\\module\\node_modules\\@mono\\false'))).toBe(false);
-  // });
+  testif(process.platform === 'win32')('should return correct value on Windows systems', () => {
+    const testStrings = ['test', '@mono/module', '@mono/sub/module'];
+    const matcherInstance = createWebpackMatcher(testStrings);
+
+    expect(matcherInstance(path.normalize('C:\\module\\node_modules\\test\\test.ext'))).toBe(true);
+    expect(matcherInstance(path.normalize('C:\\module\\node_modules\\@mono\\module\\foo'))).toBe(true);
+    expect(matcherInstance(path.normalize('C:\\module\\node_modules\\@mono\\sub\\module\\foo'))).toBe(true);
+    expect(matcherInstance(path.normalize('C:\\module\\node_modules\\@mono\\false\\module'))).toBe(false);
+    expect(matcherInstance(path.normalize('C:\\module\\node_modules\\@mono\\false'))).toBe(false);
+  });
 });
