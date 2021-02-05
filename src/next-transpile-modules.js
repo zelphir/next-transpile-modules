1f06bcfa7f6c2696a6973c34e370556f1d10bc42
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2021-02-05 15:56:05 +0100

    Adapt plugin JSDoc

--- src/next-transpile-modules.js
+++ src/next-transpile-modules.js
@@ -129,7 +129,7 @@ const createWebpackMatcher = (modulesToTranspile, logger = createLogger(false)) 
 /**
  * Transpile modules with Next.js Babel configuration
  * @param {string[]} modules
- * @param {{resolveSymlinks?: boolean, debug?: boolean, unstable_webpack5?: boolean}} options
+ * @param {{resolveSymlinks?: boolean, debug?: boolean}} options
  */
 const withTmInitializer = (modules = [], options = {}) => {
   const withTM = (nextConfig = {}) => {
